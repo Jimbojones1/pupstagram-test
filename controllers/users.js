a39605211c474af7f67db4aa9c7f190567a432fb
JimHaff <james.haff@generalassemb.ly>
2023-02-01 14:36:56 -0600

    added code

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,7 @@ const s3 = new S3();
 import { v4 as uuidv4 } from 'uuid';
 
 // So we don't have to worry about people having different bucket names we'll make the bucketname an environment variable
-const BUCKET_NAME = process.env.BUCKET_NAME
+const BUCKET_NAME = process.env.CYCLIC_BUCKET_NAME || process.env.BUCKET_NAME
 console.log(BUCKET_NAME, 'bucketname')
 
 
